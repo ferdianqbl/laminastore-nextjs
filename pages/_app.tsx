7710314150484535db8bb0101f2c920b868bae19
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-01-20 13:57:47 +0700

    convert html to next signupsuccess page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import "../styles/detail.css";
 import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
-import "../styles/sign-up-photo-success.css";
 import "../styles/sign-up-photo.css";
 import "../styles/sign-up-success.css";
 import "../styles/sign-up.css";
