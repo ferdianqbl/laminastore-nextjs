98b830bb6bac2df6f3bc9f50b76b93c5242e88bf
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-01-25 21:37:37 +0700

    refactor detail page

--- pages/detail/index.tsx
+++ pages/detail/index.tsx
@@ -1,43 +1,49 @@
 import Image from "next/image";
 import React from "react";
+import Footer from "../../src/components/organism/Footer";
+import Navbar from "../../src/components/organism/Navbar";
 import TopUpForm from "../../src/components/organism/TopUpForm";
 import TopUpItem from "../../src/components/organism/TopUpItem";
 
 export default function Detail() {
   return (
-    <section className="detail pt-lg-60 pb-50">
-      <div className="container-xxl container-fluid">
-        <div className="detail-header pb-50">
-          <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10">
-            Top Up
-          </h2>
-          <p className="text-lg color-palette-1 mb-0">
-            Perkuat akun dan jadilah pemenang
-          </p>
-        </div>
-        <div className="row">
-          <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-            <div className="row align-items-center">
-              <div className="col-md-12 col-4">
-                <Image
-                  src="/img/Thumbnail-3.png"
-                  width="280"
-                  height="380"
-                  className="img-fluid"
-                  alt=""
-                />
-              </div>
+    <>
+      <Navbar />
+      <section className="detail pt-lg-60 pb-50">
+        <div className="container-xxl container-fluid">
+          <div className="detail-header pb-50">
+            <h2 className="text-4xl fw-bold color-palette-1 text-start mb-10">
+              Top Up
+            </h2>
+            <p className="text-lg color-palette-1 mb-0">
+              Perkuat akun dan jadilah pemenang
+            </p>
+          </div>
+          <div className="row">
+            <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
+              <div className="row align-items-center">
+                <div className="col-md-12 col-4">
+                  <Image
+                    src="/img/Thumbnail-3.png"
+                    width="280"
+                    height="380"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
 
-              <TopUpItem category="desktop" />
+                <TopUpItem category="desktop" />
+              </div>
+            </div>
+            <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
+              <TopUpItem category="mobile" />
+              <hr />
+              <TopUpForm />
             </div>
           </div>
-          <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-            <TopUpItem category="mobile" />
-            <hr />
-            <TopUpForm />
-          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <Footer />
+    </>
   );
 }
