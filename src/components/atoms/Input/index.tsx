13f43365deb428f041a23461fbe982b4978e340f
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-02-09 13:02:40 +0700

    refactor edit profile

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,34 @@
+interface InputProps {
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
+}
+
+export default function Input({
+  label,
+  name,
+  type,
+  placeholder,
+  ...nativeProps
+}: InputProps) {
+  return (
+    <>
+      <label
+        htmlFor={name}
+        className="form-label text-lg fw-medium color-palette-1 mb-10"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        className="form-control rounded-pill text-lg"
+        id={name}
+        name={name}
+        aria-describedby={name}
+        placeholder={placeholder}
+        {...nativeProps}
+      />
+    </>
+  );
+}
