18213c9f7f26474c1862fd95e21ac635769a2467
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-03-16 19:10:33 +0700

    create private route for transactions dashboard

--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -1,3 +1,6 @@
+import { toast } from "react-toastify";
+import { getTokenFromCookiesAndDecodeForServer } from "../../../config/token";
+import { UserTypes } from "../../../services/data-types";
 import Sidebar from "../../../src/components/organism/Sidebar";
 import TransactionContent from "../../../src/components/organism/TransactionContent";
 
@@ -8,4 +11,32 @@ export default function Transactions() {
       <TransactionContent />
     </section>
   );
+}
+
+export async function getServerSideProps({ req }: { req: any }) {
+  const { tkn } = req.cookies;
+  if (!tkn) {
+    toast.error("Please login first", {
+      position: "top-center",
+      theme: "colored",
+    });
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  }
+  const payload = getTokenFromCookiesAndDecodeForServer(tkn);
+  const user: UserTypes = payload.player;
+
+  if (user.avatar)
+    user.avatar = `${process.env.NEXT_PUBLIC_ROOT_IMG}/player/${user.avatar}`;
+  else user.avatar = "https://source.unsplash.com/random/40x40/?person";
+
+  return {
+    props: {
+      user,
+    },
+  };
 }
